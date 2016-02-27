b75a94c29c121eaf746078e6994151a3f1bc193b
daw2eespinosa <daw2eespinosa@iesjoanramis.org>
2016-02-27 02:29:38 +0100

    Add a generic function

--- app/scripts/dwecProject.dashboard.itemsTable.js
+++ app/scripts/dwecProject.dashboard.itemsTable.js
@@ -5,7 +5,6 @@
     if (!$.dashboard) {
         $.dashboard = new Object();
     }
-    ;
     $.dashboard.itemsTable = function (el, getData, options) {
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
@@ -18,80 +17,154 @@
         base.init = function () {
             base.getData = getData;
             base.options = $.extend({}, $.dashboard.itemsTable.defaultOptions, options);
-            console.log(base.options);
-            console.log(this.$el);
 
-            console.log(base.options.template.beginContainer);
-            base.$el.append(base.options.template.beginContainer);
-            //base.el.append(base.options.template.beginAlgo1);
-            //base.el.append(base.options.template.beginAlgo2);
-            //base.el.append(base.options.template.icon);
-            //base.el.append(base.options.template.title);
-            //base.el.append(base.options.template.endAlgo2);
-            //base.el.append(base.options.template.beginFullScreen);
-            //base.el.append(base.options.template.buttonFullScreen);
-            //base.el.append(base.options.template.endFullScreen);
+            var out = base.options.template.portlet;
+            out = base.replace(out, {portletTitle: base.options.template.portletTitle});
+            out = base.replace(out, {
+                caption: base.options.template.caption,
+                actions: base.options.template.actions,
+                nav: base.options.template.nav
+            });
+            out = base.replace(out, {
+                icon: base.options.template.icon,
+                subject: base.options.template.subject,
+                buttonFullScreen: base.options.template.buttonFullScreen
+            });
+            out = base.replace(out, {
+                iconClass: base.options.iconClass,
+                fontClass: base.options.fontClass,
+                title: base.options.title
+            });
 
-            //base.el.append(base.options.template.endContainer);
+            //TODO: function to build tabs
+            //Begin examples
+            out = base.replace(out, {
+                active: base.options.template.active,
+                tab: base.options.template.tab
+            });
 
-            //base.$el.html(base.replace(base.options.template.beginContainer));
+            // Create the containers
+            out = base.replace(out, {
+                tabs: base.tabs(base.options.tabs)
+            });
 
+
+            // Call their objects
 
             // Put your initialization code here
+            base.$el.html(out);
+
+            base.getJ('http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/', base.func, undefined);
         };
-
-
-        // Sample Function, Uncomment to use
-        // base.functionName = function(paramaters){
-        //
-        // };
 
         /**
          * Replace function
          */
         base.replace = function (string, values) {
             'use strict';
-            var key;
-            var rE;
-
-            for (key in values) {
-                rE = new RegExp("({" + key + "})", "g");
-                string = string.replace(rE, values[key]);
+            //TODO: continue here 2016/02/25
+            if (values.__proto__ === Object.prototype) {
+                var key;
+                var rE;
+                for (key in values) {
+                    rE = new RegExp("{" + key + "}", "g");
+                    string = string.replace(rE, values[key]);
+                }
             }
             return string;
         };
-
-
-
+        base.tabs = function (tabs) {
+            var out = '';
+            var key;
+            for (key in tabs) {
+                if (tabs[key].type == 'tab') {
+                    out += base.replace(base.options.template.tab, {title: tabs[key].title});
+                }
+                else {
+                    out += base.replace(base.options.template.active, {title: tabs[key].title});
+                }
+            }
+            return out;
+        };
+        base.getJ = function (url, f, options) {
+            'use strict';
+            $.ajax(
+                {
+                    url: url,
+                    dataType: 'json',
+                    statusCode: {
+                        200: function (doc) {
+                            f(doc, options);
+                        }
+                    }
+                }
+            );
+        };
+        base.func = function (a, opts) {
+            console.log(typeof a);
+            console.log("doing: ", a);
+            console.log("doing: ", opts);
+        };
         // Run initializer
         base.init();
     };
     $.dashboard.itemsTable.defaultOptions = {
         template: {
-            beginContainer: '<div class="portlet light ">',
-            beginAlgo1: '<div class="portlet-title tabbable-line">',
-            beginAlgo2: '<div class="caption">',
+            portlet: '<div class="portlet light ">{portletTitle}</div>',
+            portletTitle: '<div class="portlet-title tabbable-line">{caption}{actions}{nav}</div>',
+            portletBody: '<div class="portlet-body"></div>',
+            tabContent: '<div class="tab-content">{panes}</div>',
+            caption: '<div class="caption">{icon} {subject}</div>',
             icon: '<i class="{iconClass} {fontClass}"></i>',
-            title: '<span class="caption-subject {fontClass} bold uppercase">{title}</span>',
-            endAlgo2: '</div>',
-            beginFullScreen: '<div class="actions">',
+            subject: '<span class="caption-subject {fontClass} bold uppercase">{title}</span>',
+            actions: '<div class="actions">{buttonFullScreen}</div>',
             buttonFullScreen: '<a class="btn btn-circle btn-icon-only btn-default fullscreen" href="#" data-original-title="" title=""> </a>',
-            endFullScreen: '</div>',
-            beginTabs: '<ul class="nav nav-tabs">',
-            tabs: {
-                active: '<li class="active"><a href="#tabMyTable2" data-toggle="tab" role="tab" class="active">{tabActive}</a></li>',
-                tab: '<li><a href="#myTable2" data-toggle="tab" role="tab">{tab}</a></li>'
-            },
-            endTabs: '</ul>',
-            endAlgo1: '</div>',
-            endContainer: '</div>'
+            nav: '<ul class="nav nav-tabs">{tabs}</ul>',
+            active: '<li class="active"><a href="#{id}" data-toggle="tab" role="tab" class="active">{title}</a></li>',
+            tab: '<li><a href="{id}" data-toggle="tab" role="tab">{title}</a></li>',
+            paneActive: '<div class="tab-pane active" id="tab_1_1">',
+            paneTab: '<div class="tab-pane active" id="tab_1_1">'
         },
         iconClass: "icon-globe",
         fontClass: 'font-green-sharp',
         title: "PRÓXIMOS OBJETIVOS",
-        tabs: true,
         fullScreen: true,
-        height: "relative"
+        height: "relative",
+        tabs: [
+            {
+                title: 'Generic',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/GENERIC',
+                type: 'active'
+            }, {
+                title: 'Primary',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/GOAL_PRIMARY',
+                type: 'tab'
+            }, {
+                title: 'Secondary',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/GOAL_SECUNDARY',
+                type: 'tab'
+            }, {
+                title: 'Other',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/GOAL_OTHER',
+                type: 'tab'
+            }, {
+                title: 'Session',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/SESSION',
+                type: 'tab'
+            }, {
+                title: 'Macro',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/MACROCICLE',
+                type: 'tab'
+            }, {
+                title: 'Meso',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/MESOCICLE',
+                type: 'tab'
+            }, {
+                title: 'Micro',
+                url: 'http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/MICROCICLE',
+                type: 'tab'
+            }
+        ]
     };
     $.fn.myCorp_MyExample = function (getData, options) {
         return this.each(function () {
