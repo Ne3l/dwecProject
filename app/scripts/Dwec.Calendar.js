c78cac56dc8d2543ec03dea5bd801d06561d4dce
jBenejam <daw2jbenejam@iesjoanramis.org>
2016-03-03 20:40:09 +0100

    Version 2.0

--- app/scripts/Dwec.Calendar.js
+++ app/scripts/Dwec.Calendar.js
@@ -1,4 +1,7 @@
-//Añadir content al añadir evento (Mirar nueva version Postman)
+//Autores Jordi Benejam - Nacho Lopez
+//Funcionalidad Calen
+
+
 (function ($) {
   if (!$.Dwec) {
     $.Dwec = new Object();
@@ -9,47 +12,55 @@
     base.$el = $(el);
     base.el = el;
     base.$el.data("Dwec.Calendar", base);
+
+    base.$eventBox = $('#event_box');
+
     base.init = function () {
       base.getData = getData;
       base.options = $.extend({}, $.Dwec.Calendar.defaultOptions, options);
 
-
       $("body").append("<div id='myModal' class='modal fade' tabindex='-1' role='dialog'> <div class='modal-dialog'> <div class='modal-content'> <div class='modal-header'> <button type='button' class='close' data-dismiss='modal' aria-label='Closev><span aria-hidden='true'>&times;</span></button> <h4 class='modal-title'>Information</h4> </div> <div class='modal-body'> <p id='content'></p> </div> <div class='modal-footer'> <button id='close' type='button' class='btn btn-default' data-dismiss='modal'>Close</button> <button type='button' id='accept' class='btn btn-primary' data-dismiss='modal'>Save changes</button> </div> </div></div></div>");
-      base.renderGenericEvents();
+      //base.renderGenericEvents();
       base.renderBin();
       base.renderCalendar();
     };
 
+    /**
+     * Generic method to petition/update events in Server
+     * @param url
+     * @param method
+     * @param sendData
+     * @param fOnSuccessCallback
+     * @param fOnErrorCallback
+       */
     base.doAjax = function (url, method, sendData, fOnSuccessCallback, fOnErrorCallback) {
-      //var item = {error:"200","func":function(){}}; se me va la olla
       var callBackFunctions = {
-        200: function (response) {
+        200: function _(response) {
           console.log("Codigo 200 respuesta");
           console.log(response);
-          fOnSuccessCallback(response.id);
+          fOnSuccessCallback(response);
         },
-        201: function (response) {
+        201: function _(response) {
           console.log("Codigo 201 respuesta");
           console.log(response);
-          fOnSuccessCallback(response.id);
+          fOnSuccessCallback(response);
         },
-        204: function (response) {
-          console.log("Codigo 201 respuesta");
+        204: function _(response) {
+          console.log("Codigo 204 respuesta");
           console.log(response);
           fOnSuccessCallback();
         },
-        400: function (response) {
+        400: function _(response) {
           console.log("Codigo 400 respuesta");
           console.log(response);
           fOnErrorCallback();
-
         },
-        404: function (response) {
+        404: function _(response) {
           console.log("Codigo 404 respuesta");
           console.log(response);
           fOnErrorCallback();
         },
-        500: function (response) {
+        500: function _(response) {
           console.log("Codigo 500 respuesta");
           console.log(response);
           fOnErrorCallback();
@@ -69,6 +80,12 @@
     };
 
     base.addOnDragOverDays = function () {
+
+      //TODO Si selectior tiene id wrapper => ya puedo tener variar instancias
+      // TODO $('.fc-day') => $('#' + containerID + '.fc-day');
+      // TODO Container ID es generado por mi => Preguntar Llorenc;
+
+
       // fc-day-number alternative selector
       $('.fc-day').each(function (key, value) {
         $(this).on("dragover", function (event) {
@@ -98,26 +115,26 @@
             "description": "Descripcion del Evento",
             "startDate": fechaStart,
             "endDate": fechaEnd,
-            "eventType": "GENERIC",
-            "status": "NOT_DEFINED",
+            "eventType": "SESSION", //TODO coger de options
+            "status": "NOT_DEFINED", // TODO coger de options o de la informacion del propio generico que arrastro
             "eventGroup": {"id": event.originalEvent.dataTransfer.getData('idGeneric')},
             "content": {
-              "data": [
-                {"label": "Prueba", "note": "Loren ipsum"},
-                {"label": "Prueba", "note": "Loren ipsum"}
-              ]
+              "data": [{"label": "Prueba", "note": "Loren ipsum"}, {"label": "Prueba", "note": "Loren ipsum"}]
             },
             "configuration": {
-              "data": [
-                {"conceptType": "INTENSITY", "value": 8, "label": "Prueba", "note": "Loren ipsum"},
-                {"conceptType": "VOLUME", "value": 8, "label": "Prueba", "note": "Loren ipsum"}
-              ]
+              "data": [{
+                "conceptType": "INTENSITY",
+                "value": 8,
+                "label": "Prueba",
+                "note": "Loren ipsum"
+              }, {"conceptType": "VOLUME", "value": 8, "label": "Prueba", "note": "Loren ipsum"}]
             }
           };
 
-          var fOnSuccessCallback = function (id) {
+          var fOnSuccessCallback = function fOnSuccessCallback(response) {
+
             var myEvent = {
-              id: id,
+              id: response.id,
               title: "Evento " + tipoEvento,
               start: fechaStart,
               end: fechaEnd, //"2016-02-03T12:01:00"
@@ -126,17 +143,14 @@
             $(el).fullCalendar('renderEvent', myEvent);
             toastr.success("Evento añadido", "Evento dia " + myEvent.start);
           };
-          var fOnErrorCallback = function () {
-            toastr.error("Ups!", "No podemos conectar...");
-          };
-
-          base.doAjax(url, 'POST', sendData, fOnSuccessCallback, fOnErrorCallback);
+          base.doAjax(url, 'POST', sendData, fOnSuccessCallback, base.options.connectionFail);
         });
       });
     };
 
     base.renderBin = function () {
-      var $bin = '<div class="col-md-12" style="padding-bottom:10px;text-align:left;display:flex;align-items:center;margin-bottom: 20px; border-bottom: 1px solid #e5e5e5 ;"><a href="javascript:;" id="event_add" class="btn green"> Add Event </a><span id="bin" class="glyphicon glyphicon-trash" style="color:#BBDEFB;font-size:3em;"></span></div><span class="divider"></span> ';
+      // TODO revisar IDs
+      var $bin = '<div class="col-md-12" style="padding-bottom:10px; padding-left:0px ;text-align:left;display:flex;align-items:center;margin-bottom: 20px; border-bottom: 1px solid #e5e5e5 ;"><a href="javascript:;" id="event_add" class="btn green"> Add Event </a><span id="bin" class="glyphicon glyphicon-trash" style="color:#BBDEFB;font-size:3em;"></span></div>';
       $('#external-events').prepend($bin);
 
       $(bin).on("dragover", function (event) {
@@ -150,54 +164,59 @@
         event.stopPropagation();
         this.style.color = '#BBDEFB';
       });
-
-
     };
 
-    base.renderGenericEvents = function () {
+    base.isEventOverDiv = function (x, y) {
+      // TODO Revisar IDs
+      var bin = $('#bin');
+      var offset = bin.offset();
+      offset.right = bin.width() + offset.left;
+      offset.bottom = bin.height() + offset.top;
+      console.log(offset.left);
+      console.log(offset.top);
+      console.log(offset.right);
+      console.log(offset.bottom);
+      // Compare
+      if (x >= offset.left && x <= offset.right) {
+        return true;
+      }
+      return false;
+    };
+
+    base.renderGenericEvents = function (genericEvent) {
       var divEvents = '<div style="position: relative;" class="{classes}" {attr} draggable="true">{name}</div>';
       var classes = 'external-event label label-default ui-draggable ui-draggable-handle';
 
-      var onDragStart = function (event) {
-        console.log(event);
+      var onDragStart = function onDragStart(event) {
         event.originalEvent.dataTransfer.setData('tipoEvento', event.currentTarget.innerHTML);
         event.originalEvent.dataTransfer.setData('idGeneric', event.currentTarget.attributes[2].nodeValue);
         event.originalEvent.dataTransfer.setData('colorGeneric', event.currentTarget.style.backgroundColor);
       };
 
-      var $box = $('#event_box');
-
-      $.ajax({
-        url: base.options.urlEventsTypes,
-        dataType: 'json',
-        success: function (doc) {
-          $.each(doc, function () {
-            $box.append($(divEvents.replace("{classes}", classes).replace("{attr}", "data-id='" + this.id + "'").replace("{name}", this.name)).on('dragstart', onDragStart).css("background-color", this.additionalInfo));
-            console.log(this);
-          });
-
-        },
-        error: function (doc) {
-          toastr.error("La peticion ajax ha fallado");
-        }
-      });
+      base.$eventBox.append($(divEvents.replace("{classes}", classes)
+        .replace("{attr}", "data-id='" + genericEvent.eventGroup.id + "'")
+        .replace("{name}", genericEvent.name))
+        .on('dragstart', onDragStart)
+        .css("background-color", genericEvent.content.data[0].value));
 
     };
 
     base.changeDay = function (event, revertFunc, info, success, error) {
+      // TODO revisar ID's
       $("#content").text(info);
       $("#accept").on("click", function () {
         var url = base.options.urlEdit + event.id;
         var sendData = {
           id: event.id,
           "startDate": event.start,
-          "endDate": event.end
+          "endDate": event.end,
+          "eventType": "SESSION"
         };
 
-        var fOnSuccessCallback = function (id) {
+        var fOnSuccessCallback = function fOnSuccessCallback(id) {
           toastr.success(success.title, success.description);
         };
-        var fOnErrorCallback = function () {
+        var fOnErrorCallback = function fOnErrorCallback() {
           revertFunc();
           toastr.error(error.title, error.description);
         };
@@ -211,27 +230,11 @@
       $('#myModal').modal('show');
     };
 
-    base.isEventOverDiv = function(x, y) {
-      var bin = $('#bin');
-      var offset = bin.offset();
-      offset.right = bin.width() + offset.left;
-      offset.bottom = bin.height() + offset.top;
-          console.log(offset.left);
-      console.log(offset.top);
-      console.log(offset.right);
-      console.log(offset.bottom);
-      // Compare
-      if (x >= offset.left &&
-        //y >= offset.top &&
-        x <= offset.right /*&&
-        y <= offset.bottom*/) {
-        return true;
-      }
-      return false;
-
-    };
-
     base.renderCalendar = function () {
+
+      //TODO todos los mensajes en options -> i18n
+
+
       $(el).fullCalendar({
         firstDay: 1,
         droppable: true,
@@ -241,42 +244,18 @@
           left: 'title', center: 'month,agendaWeek', right: 'today prev,next'
         },
         eventLimit: true,
-        viewRender: function () {
+        viewRender: function viewRender() {
           base.addOnDragOverDays();
         },
-        eventDrop: function (event, delta, revertFunc) {
+        eventDrop: function eventDrop(event, delta, revertFunc) {
           console.log(event);
 
           var info = event.title + " was dropped on " + event.start.format();
           var success = {title: "Evento modificado", description: "Evento dia " + event.start.format()};
           var error = {title: "Ups!", description: "No podemos conectar..."};
           base.changeDay(event, revertFunc, info, success, error);
-          //alert(event.title + " was dropped on " + event.start.format());
-          //if (!confirm("Are you sure about this change?")) {
-          //    revertFunc();
-          //} else {
-          //    var url = base.options.urlEdit + event.id;
-          //    var sendData = {
-          //        id: event.id,
-          //        "startDate": event.start,
-          //        "endDate": event.end,
-          //        "content": {"data": []},
-          //        "configuration": {"data": []}
-          //    };
-          //
-          //    var fOnSuccessCallback = function (id) {
-          //        toastr.success("Evento añadido", "Evento dia " + event.start);
-          //    };
-          //    var fOnErrorCallback = function () {
-          //        revertFunc();
-          //        toastr.error("Ups!", "No podemos conectar...");
-          //    };
-          //
-          //    base.doAjax(url, 'PUT', sendData, fOnSuccessCallback, fOnErrorCallback);
-          //    console.log(JSON.stringify(event));
-          //}
         },
-        eventResize: function (event, delta, revertFunc) {
+        eventResize: function eventResize(event, delta, revertFunc) {
           var info = event.title + " end is now " + event.end.format();
           var success = {
             title: "Evento modificado",
@@ -285,64 +264,55 @@
           var error = {title: "Ups!", description: "No podemos conectar..."};
 
           base.changeDay(event, revertFunc, info, success, error);
-          //alert(event.title + " end is now " + event.end.format());
-          //if (!confirm("is this okay?")) {
-          //    revertFunc();
-          //}
         },
-        eventDragStop: function(event, jsEvent, ui, view) {
-          console.log("X -> "+jsEvent.clientX);
-          console.log("Y -> "+jsEvent.clientY);
+        eventDragStop: function eventDragStop(event, jsEvent, ui, view) {
+          console.log("X -> " + jsEvent.clientX);
+          console.log("Y -> " + jsEvent.clientY);
           if (base.isEventOverDiv(jsEvent.clientX, jsEvent.clientY)) {
+            console.log("as");
 
             $('#calendar').fullCalendar('removeEvents', event._id);
             var fOnSuccessCallback = function (id) {
-              toastr.success("Evento "+event.title, "Eliminado");
+              toastr.success("Evento " + event.title, "Eliminado");
             };
             var fOnErrorCallback = function () {
-              toastr.error("Evento "+event.title, "Error al eliminar");
+              toastr.error("Evento " + event.title, "Error al eliminar");
             };
 
-            base.doAjax(base.options.urlDel+event._id, 'DELETE', "", fOnSuccessCallback, fOnErrorCallback);
+            base.doAjax(base.options.urlDel + event._id, 'DELETE', "", fOnSuccessCallback, fOnErrorCallback);
           }
         },
-        events: function (start, end, timezone, callback) {
-          $.ajax({
-            url: base.options.urlGet,
-            dataType: 'json',
-            data: {
-              start: start.unix(),
-              end: end.unix()
-            },
-            success: function (doc) {
-              var events = [];
-              for (var item in doc) {
-                if (doc[item]['startDate'] != null)
+        events: function events(start, end, timezone, callback) {
+          var fOnSuccessCallback = function fOnSuccessCallback(doc) {
+            var events = [];
+            for (var item in doc) {
+              if (doc[item]["eventType"]["itemLabel"] === "generic") {
+                base.renderGenericEvents(doc[item]);
+              } else {
+                if (doc[item]['startDate'] != null) {
                   events.push({
                     id: doc[item]['id'],
                     title: doc[item]['name'],
                     start: doc[item]['startDate'],
                     end: doc[item]['endDate'],
                     allDay: true
-                  })
+                  });
+                }
               }
-              callback(events);
-            },
-            error: function (doc) {
-              toastr.error("La peticion ajax ha fallado");
             }
-          });
+            callback(events);
+          };
+          base.doAjax(base.options.urlGet, "GET", null, fOnSuccessCallback, base.options.connectionFail);
         },
         eventColor: '#32c5d2'
       });
-
     };
 
     base.init();
   };
   $.Dwec.Calendar.defaultOptions = {
+    // TODO cambiar nombres urls
     html: true,
-    url: "http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/",
     urlEdit: "http://tomcat7-mycoachgate.rhcloud.com/rest/events/set/",
     urlGet: "http://tomcat7-mycoachgate.rhcloud.com/rest/events/get/",
     urlAdd: "http://tomcat7-mycoachgate.rhcloud.com/rest/events/add/",
@@ -352,22 +322,25 @@
     post: "",
     msgError: "Content not found",
     eventsJson: {},
-    editOnSuccess: function () {
+    connectionFail: function connectionFail() {
+      toastr.error("Ups!", "No podemos conectar...");
     },
-    editOnFail: function () {
+    editOnSuccess: function editOnSuccess() {
     },
-    addOnSuccess: function () {
+    editOnFail: function editOnFail() {
     },
-    addOnFail: function () {
+    addOnSuccess: function addOnSuccess() {
     },
-    deleteOnSuccess: function () {
+    addOnFail: function addOnFail() {
     },
-    deleteOnFail: function () {
+    deleteOnSuccess: function deleteOnSuccess() {
+    },
+    deleteOnFail: function deleteOnFail() {
     }
   };
   $.fn.Dwec_Calendar = function (getData, options) {
     return this.each(function () {
-      (new $.Dwec.Calendar(this, getData, options));
+      new $.Dwec.Calendar(this, getData, options);
     });
   };
 
